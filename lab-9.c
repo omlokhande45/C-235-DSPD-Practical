3a13a225a68ebe85c802a301f7f30a45888b1ca9
omlokhande45 <omlokhande929@gmail.com>
2025-11-08 01:09:29 +0530

    Update lab-9.c

--- lab-9.c
+++ lab-9.c
@@ -1,24 +1,53 @@
-#include<stdio.h>
-int main(){
-    int hash[7],i,key,index,n;
-    for(i=0;i<7;i++){
-        hash[i]=-1;
-        printf("Enter number of keys to insert : ");
-        scanf("%d",&n);
-        for(i=0;i<n;i++){
-            printf("Enter key : ");
-            scanf("%d",&key);
-            index=key%7;
-            while(hash[index]!=-1){
-                index=(index + 1)%7;
-            }
-                hash[index]=key;
+#include <stdio.h>
+#define size 7
 
-            
+int arr[size];
+
+void init() {
+    for (int i = 0; i < size; i++)
+        arr[i] = -1;
+}
+
+int h(int k) {
+    return k % size;
+}
+
+void insert(int k) {
+    int i = h(k);
+    int s = i;
+    while (arr[i] != -1) {
+        i = (i + 1) % size;
+        if (i == s) {
+            printf("full! %d\n", k);
+            return;
         }
     }
-    for(i=0;i<7;i++){
-        printf("Slot %d : %d\n",hash[i]);
+    arr[i] = k;
+    printf("added %d at %d\n", k, i);
+}
+
+void show() {
+    printf("\nslot : val\n");
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == -1)
+            printf("%2d : empty\n", i);
+        else
+            printf("%2d : %d\n", i, arr[i]);
     }
+}
+
+int main() {
+    int n, k;
+    init();
+    printf("how many keys: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("key %d: ", i + 1);
+        scanf("%d", &k);
+        insert(k);
+    }
+
+    show();
     return 0;
 }
